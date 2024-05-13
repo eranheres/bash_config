7ae8bc02647298f41ed429683fc8058975a543ae
eran.heres@gmail.com <eran.heres@gmail.com>
2024-05-13 15:07:31 +0300

    adding brew and iterm profile

--- zsh_profile.zsh
+++ zsh_profile.zsh
@@ -1,13 +1,15 @@
-export OPENAI_API_KEY_NVIM=$(grep OPENAI_API_KEY_NVIM $(dirname "$0")/config.yml | cut -d' ' -f2)
+#export OPENAI_API_KEY_NVIM=$(grep OPENAI_API_KEY_NVIM $(dirname "$0")/config.yml | cut -d' ' -f2)
 export OPENAI_API_KEY=$(grep OPENAI_API_KEY $(dirname "$0")/config.yml | cut -d' ' -f2)
 [ -f ~/.lazyshell.zsh ] && source ~/.lazyshell.zsh
 alias nvenv='python3 -m venv .venv && source .venv/bin/activate && pip install --upgrade pip'
 alias avenv='source .venv/bin/activate'
 
-alias nvim="NVIM_APPNAME=nvim nvim"
-alias nvim-ios="NVIM_APPNAME=nvim-ios nvim"
-alias nvim-py="NVIM_APPNAME=nvim-python nvim"
-alias nvim-ios-starter="NVIM_APPNAME=nvim-ios-starter nvim"
+alias nvim-a='NVIM_APPNAME=nvim-a nvim'
+alias nvim-c='NVIM_APPNAME=nvim-c nvim'
+alias nvim-ios='NVIM_APPNAME=nvim-ios \nvim'
+alias nvim-py='NVIM_APPNAME=nvim-python \nvim'
+alias nvim-common='NVIM_APPNAME=nvim-common \nvim'
+alias nvim-ios-starter='NVIM_APPNAME=nvim-ios-starter \nvim'
 
 source /Users/eranheres/.config/nvims/nvim_appnames
 
