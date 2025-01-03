07435942f5042bad5f3b1fe08e563ec857741b87
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-03 21:46:56 +0200

    wip more

--- dockers/my-nvim/init_docker.sh
+++ dockers/my-nvim/init_docker.sh
@@ -1,5 +1,6 @@
 cd /root/bash_config
 git pull
+cd /root/.config/nvim-obsidian
+git pull
 cd /root
 git clone https://$(echo $GITHUB_TOKEN | tr -d '\n')@github.com/eranheres/second-brain.git ~/second-brain
-/bin/zsh
