7ba0fc1060e29c7881eaa69b0cd0d1fa386fde56
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-03 20:59:44 +0200

    .

--- dockers/my-nvim/docker_local_run_command.sh
+++ dockers/my-nvim/docker_local_run_command.sh
@@ -1,2 +1,2 @@
 docker pull --platform linux/amd64 eranheres1/my-nvim:latest 
-docker run -it --platform linux/amd64 eranheres1/my-nvim:latest /bin/zsh
+docker run -it --platform linux/amd64 -e GITHUB_TOKEN=$(cat ~/.ssh/github-token.txt) eranheres1/my-nvim:latest
