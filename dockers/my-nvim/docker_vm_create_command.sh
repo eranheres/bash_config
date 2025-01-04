b572252b8eeacb18ca6940f79545ed7159a6c5ae
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 18:11:28 +0200

    adding zone

--- dockers/my-nvim/docker_vm_create_command.sh
+++ dockers/my-nvim/docker_vm_create_command.sh
@@ -14,4 +14,5 @@ gcloud compute instances create-with-container my-nvim \
   --container-env GITHUB_TOKEN=$REPO_TOKEN
 
 gcloud compute ssh my-nvim \
+  --zone us-central1-b \
   --command='echo "docker exec -it \$(docker ps -q | head -n 1) /bin/zsh -c ./bash_config/dockers/my-nvim/run_obs.sh" > ~/obs'
