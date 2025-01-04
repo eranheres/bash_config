ddf3140507cf73ae5c0cdeb062e80a06fbd9cf6b
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 15:07:58 +0200

    wip

--- dockers/my-nvim/docker_vm_create_command.sh
+++ dockers/my-nvim/docker_vm_create_command.sh
@@ -1,12 +1,12 @@
 ## if parameter provided, set it as github token otherwise use the default one
 if [ -z "$1" ]; then
-  GITHUB_TOKEN=$(cat ~/.ssh/github-token.txt)
+  REPO_TOKEN=$(cat ~/.ssh/github-token.txt)
 else
-  GITHUB_TOKEN=$1
+  REPO_TOKEN=$1
 fi
 gcloud compute instances create-with-container my-nvim \
   --container-image docker.io/eranheres1/my-nvim:latest \
   --container-restart-policy always \
   --container-stdin --container-tty \
   --address=35.232.155.126 \
-  --container-env GITHUB_TOKEN=$GIT_TOKEN
+  --container-env GITHUB_TOKEN=$REPO_TOKEN
