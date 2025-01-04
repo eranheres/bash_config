2ef5b66b2e5de2533952dd10e75485742b27ecdb
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 17:43:17 +0200

    run obs script

---
+++ dockers/my-nvim/run_obs.sh
@@ -0,0 +1,5 @@
+export PATH="$PATH:/opt/nvim-linux64/bin"
+source /root/bash_config/zsh_profile.zsh
+export SECOND_BRAIN_DIR="/root/second-brain"
+cd /root/second-brain
+NVIM_APPNAME=nvim-obsidian nvim .
