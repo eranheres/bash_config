d5d65da91d63517ad4ef0eb7c0b8ddd30e9c4d02
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 13:51:40 +0200

    wip

--- dockers/my-nvim/docker_vm_create_command.sh
+++ dockers/my-nvim/docker_vm_create_command.sh
@@ -2,8 +2,5 @@ gcloud compute instances create-with-container my-nvim \
   --container-image docker.io/eranheres1/my-nvim:latest \
   --container-restart-policy always \
   --container-stdin --container-tty \
-  --container-command "/bin/bash" \
-  --container-arg "-c" \
-  --container-arg "while true; do sleep 60; done" \
   --address=35.232.155.126 \
   --container-env GITHUB_TOKEN=$(cat ~/.ssh/github-token.txt)
--- dockers/my-nvim/init_docker.sh
+++ dockers/my-nvim/init_docker.sh
@@ -4,8 +4,3 @@ cd /root/.config/nvim-obsidian
 git pull
 cd /root
 git clone https://$(echo $GITHUB_TOKEN | tr -d '\n')@github.com/eranheres/second-brain.git ~/second-brain
-
-echo "Container is now running..."
-while true; do
-    sleep 60
-done
