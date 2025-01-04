c933e356ae0f7c2d83a4a0d8673ec7457f51ac24
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 18:30:49 +0200

    relocate file

--- dockers/my-nvim/setup_host.sh
+++ dockers/my-nvim/setup_host.sh
@@ -1,4 +1,7 @@
 set -x
 gcloud compute ssh my-nvim \
   --zone us-central1-b \
-  --command='echo "docker exec -it \$(docker ps -q | head -n 1) /bin/zsh -c ./bash_config/dockers/my-nvim/run_obs.sh" > ~/obs.sh'
+  --command='echo "docker exec -it \$(docker ps -q | head -n 1) /bin/zsh -c ./bash_config/dockers/my-nvim/run_obs.sh" > /home/eranheres/obs.sh'
+gcloud compute ssh my-nvim \
+  --zone us-central1-b \
+  --command='chmod a+x /home/eranheres/obs.sh'
