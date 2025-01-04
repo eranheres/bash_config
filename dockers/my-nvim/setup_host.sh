109a553673cabb23f02a6e1a9c2086b39f45ed2d
eran.heres@gmail.com <eran.heres@gmail.com>
2025-01-04 18:41:34 +0200

    change file name

--- dockers/my-nvim/setup_host.sh
+++ dockers/my-nvim/setup_host.sh
@@ -1,7 +1,4 @@
 set -x
 gcloud compute ssh eranheres@my-nvim \
   --zone us-central1-b \
-  --command='echo "docker exec -it \$(docker ps -q | head -n 1) /bin/zsh -c ./bash_config/dockers/my-nvim/run_obs.sh" > /home/eranheres/obs.sh'
-gcloud compute ssh eranheres@my-nvim \
-  --zone us-central1-b \
-  --command='chmod a+x /home/eranheres/obs.sh'
+  --command='echo "docker exec -it \$(docker ps -q | head -n 1) /bin/zsh -c ./bash_config/dockers/my-nvim/run_obs.sh" > /home/eranheres/obs'
